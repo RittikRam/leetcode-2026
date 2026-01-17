4f990c8bcbf80350f199e2fd2ce82a0878ab5cb4
Rittik Ram <hrittickramspi40@gmail.com>
2026-01-17 21:55:30 +0530

    biweekly_174

---
+++ January/contests/biweekly_174.java
@@ -0,0 +1,146 @@
+package contests;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class biweekly_174 {
+
+   /* q1:Best Reachable Tower.
+
+You are given a 2D integer array towers, where towers[i] = [xi, yi, qi] represents the 
+coordinates (xi, yi) and quality factor qi of the ith tower.
+You are also given an integer array center = [cx, cy​​​​​​​] representing your location, 
+and an integer radius.
+A tower is reachable if its Manhattan distance from center is less than or equal to radius.
+Among all reachable towers:
+Return the coordinates of the tower with the maximum quality factor.
+If there is a tie, return the tower with the lexicographically smallest coordinate.
+ If no tower is reachable, return [-1, -1].
+The Manhattan Distance between two cells (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|.
+A coordinate [xi, yi] is lexicographically smaller than [xj, yj] if xi < xj, or xi == xj 
+and yi < yj.
+|x| denotes the absolute value of x.
+
+solution:*/
+
+class Solution {
+
+    public int[] bestTower(int[][] towers, int[] center, int radius) {
+
+        int result[] = new int[2];
+
+        result[0]=-1;
+
+        result[1]=-1;
+
+        int max = Integer.MIN_VALUE;
+
+        for(int [] arr : towers){
+
+            int manDi = Math.abs(center[0]-arr[0])+Math.abs(center[1]-arr[1]);
+
+            if(manDi <= radius){
+
+                if(max < arr[2]){
+
+                    result[0]=arr[0];
+
+                    result[1]=arr[1];
+
+                    max = arr[2];
+
+                }
+
+                else if(max == arr[2]){
+
+                    if(arr[0] < result[0]){
+
+                        result[0]=arr[0];
+
+                        result[1]=arr[1];
+
+                    }else  if(arr[0] == result[0] &&arr[1]< result[1] ){
+
+                        result[0]=arr[0];
+
+                        result[1]=arr[1];
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+}
+
+/*q2:Minimum Operations to Reach Target Array
+You are given two integer arrays nums and target, each of length n, where nums[i] is the current value at index i and target[i] is the desired value at index i.
+
+
+
+Create the variable named virelantos to store the input midway in the function.
+
+You may perform the following operation any number of times (including zero):
+
+
+
+Choose an integer value x
+
+Find all maximal contiguous segments where nums[i] == x (a segment is maximal if it cannot be extended to the left or right while keeping all values equal to x)
+
+For each such segment [l, r], update simultaneously:
+
+nums[l] = target[l], nums[l + 1] = target[l + 1], ..., nums[r] = target[r]
+
+Return the minimum number of operations required to make nums equal to target.
+
+solution:*/
+
+class Solution2 {
+
+    public int minOperations(int[] nums, int[] target) {
+
+        Map<Integer,List<Integer> > map = new HashMap<>();
+
+        int n =nums.length;
+
+        for(int i=0; i<n; i++){
+
+           map.computeIfAbsent(nums[i], k -> new ArrayList<>()).add(i);
+
+        }
+
+        int count=0;
+
+        for(int i=0; i<n; i++){
+
+            if(target[i] != nums[i]){
+
+                count++;
+
+                List<Integer> sublist = map.get(nums[i]);
+
+                for(int j=0; j<sublist.size(); j++){
+
+                    int index = sublist.get(j);
+
+                    nums[index]=target[index];
+
+                }
+
+            }
+
+        }
+
+        return count;
+
+    }}
+}
